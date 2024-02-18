ed5e0a9297a033b8494e1d1755e8af5cb69d0c72
Charymyrat Garryyev <kcharymyrat@gmail.com>
2024-02-18 19:33:16 +0500

    Stage 2 Done

--- QRCode Service/task/src/qrcodeapi/AppConroller.java
+++ QRCode Service/task/src/qrcodeapi/AppConroller.java
@@ -1,9 +1,18 @@
 package qrcodeapi;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.awt.image.RenderedImage;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 
 @RestController
 public class AppConroller {
@@ -14,7 +23,24 @@ public class AppConroller {
     }
 
     @GetMapping("/api/qrcode")
-    @ResponseStatus(HttpStatus.NOT_IMPLEMENTED)
-    public void getQrCode() {
+    @ResponseStatus(HttpStatus.OK)
+    public ResponseEntity<byte[]> getImage() throws IOException {
+        BufferedImage bufferedImage = new BufferedImage(250, 250, BufferedImage.TYPE_INT_RGB);
+
+        Graphics2D g = bufferedImage.createGraphics();
+        g.setColor(Color.WHITE);
+        g.fillRect(0, 0, 250, 250);
+
+        try (var baos = new ByteArrayOutputStream()) {
+            ImageIO.write(bufferedImage, "png", baos); // writing the image in the PNG format
+            byte[] bytes = baos.toByteArray();
+            return ResponseEntity
+                    .ok()
+                    .contentType(MediaType.IMAGE_PNG)
+                    .body(bytes);
+        } catch (IOException e) {
+            // handle the IOEexception
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
     }
 }
