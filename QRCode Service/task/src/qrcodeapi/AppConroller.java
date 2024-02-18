c776070855f675497e886505ffcf539f1bf554b2
Charymyrat Garryyev <kcharymyrat@gmail.com>
2024-02-18 21:56:10 +0500

    Stage 5 done. Project finished

--- QRCode Service/task/src/qrcodeapi/AppConroller.java
+++ QRCode Service/task/src/qrcodeapi/AppConroller.java
@@ -1,10 +1,13 @@
 package qrcodeapi;
 
 import com.google.zxing.BarcodeFormat;
+import com.google.zxing.EncodeHintType;
 import com.google.zxing.WriterException;
 import com.google.zxing.client.j2se.MatrixToImageWriter;
 import com.google.zxing.common.BitMatrix;
 import com.google.zxing.qrcode.QRCodeWriter;
+import com.google.zxing.qrcode.decoder.ErrorCorrectionLevel;
+import org.springframework.boot.context.properties.bind.DefaultValue;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -18,11 +21,22 @@ import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @RestController
 public class AppConroller {
 
-    QRCodeWriter writer = new QRCodeWriter();
+    private final QRCodeWriter writer = new QRCodeWriter();
+    private final Map<String, ErrorCorrectionLevel> errorCorrectionMap = new HashMap<>() {{
+        put("L", ErrorCorrectionLevel.L);
+        put("M", ErrorCorrectionLevel.M);
+        put("Q", ErrorCorrectionLevel.Q);
+        put("H", ErrorCorrectionLevel.H);
+    }};
+
 
     @GetMapping("/api/health")
     @ResponseStatus(HttpStatus.OK)
@@ -33,11 +47,12 @@ public class AppConroller {
     @ResponseStatus(HttpStatus.OK)
     public ResponseEntity<?> getImage(
             @RequestParam("contents") String contents,
-            @RequestParam("size") int size,
-            @RequestParam("type") String type
+            @RequestParam(value = "size", defaultValue = "250") int size,
+            @RequestParam(value = "correction", defaultValue = "L") String correction,
+            @RequestParam(value = "type", defaultValue = "png") String type
     ) throws IOException {
 
-        System.out.printf("\n\n\nsize = %d, type = %s, contents = %s, %s\n\n\n", size, type, contents, contents.isEmpty());
+        System.out.printf("\n\n\nsize = %d, type = %s, contents = %s, correction = %s\n\n\n", size, type, contents, correction);
 
         if (contents.trim().isEmpty()) {
             return ResponseEntity
@@ -50,6 +65,15 @@ public class AppConroller {
                     .status(HttpStatus.BAD_REQUEST)
                     .body(new CustomError("Image size must be between 150 and 350 pixels"));
         }
+
+        correction = correction.trim().toUpperCase();
+        System.out.printf("correction = %s, %d\n", correction, correction.length());
+        if (!errorCorrectionMap.containsKey(correction)) {
+            return ResponseEntity
+                    .status(HttpStatus.BAD_REQUEST)
+                    .body(new CustomError("Permitted error correction levels are L, M, Q, H"));
+        }
+
 
         MediaType mediaType;
         if (type.trim().equalsIgnoreCase("png")) {
@@ -64,8 +88,9 @@ public class AppConroller {
                     .body(new CustomError("Only png, jpeg and gif image types are supported"));
         }
 
+        Map<EncodeHintType, ?> hints = Map.of(EncodeHintType.ERROR_CORRECTION, errorCorrectionMap.get(correction));
         try (var baos = new ByteArrayOutputStream()) {
-            BitMatrix bitMatrix = writer.encode(contents.trim(), BarcodeFormat.QR_CODE, size, size);
+            BitMatrix bitMatrix = writer.encode(contents.trim(), BarcodeFormat.QR_CODE, size, size, hints);
             BufferedImage bufferedImage = MatrixToImageWriter.toBufferedImage(bitMatrix);
 
             ImageIO.write(bufferedImage, type, baos); // writing the image in the PNG format
